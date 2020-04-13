cf8fde6582cede62c056d6c777b226fecf42de1f
gustavo <gustavo.maia@b2b.com.pt>
2020-04-13 18:02:07 +0100

    PokemonViewHolder

---
+++ app/src/main/java/com/artemissoftware/pokeapi/adapters/PokemonViewHolder.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.pokeapi.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.artemissoftware.pokeapi.databinding.ItemPokemonBinding
+
+
+class PokemonViewHolder(val itemVehicleBinding: ItemPokemonBinding) : RecyclerView.ViewHolder(itemVehicleBinding.root)
