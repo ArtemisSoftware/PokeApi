ecb516afd867e1c7754dddb32429c597c1d7976a
gustavo <gustavo.maia@b2b.com.pt>
2020-04-14 16:01:02 +0100

    NetworkService

---
+++ app/src/main/java/com/artemissoftware/pokeapi/service/NetworkService.kt
@@ -0,0 +1,25 @@
+package com.artemissoftware.pokeapi.service
+
+import com.artemissoftware.pokeapi.api.PokemonApi
+import com.artemissoftware.pokeapi.models.PokedexResult
+import io.reactivex.Single
+import javax.inject.Inject
+
+class NetworkService {
+
+    @Inject
+    lateinit var pokemonApi: PokemonApi
+
+    init {
+        //DaggerApiComponent.create().inject(this)
+    }
+
+    fun fetchPokedex(): Single<PokedexResult> {
+        return pokemonApi.getPokedex()
+    }
+
+    companion object {
+        val BASE_URL = "https://pokeapi.co/api/v2/"
+    }
+
+}
